e325928c8654a53670a8d51e20caaf8fb66bc1e0
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-11-30 20:51:04 +0530

    Add Kafka Producer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 const express = require('express');
+const {Kafka} = require('kafkajs')
+
 require('dotenv').config()
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+(async () => {
+    const kafka = new Kafka({
+        clientId: 'kafka-nodejs-starter',
+        brokers: ['localhost:9092'],
+    });
+
+    const producer = kafka.producer()
+
+// Connect to the producer
+
+    await producer.connect()
+
+// Send an event to the demoTopic topic
+    await producer.send({
+        topic: 'demoTopic',
+        messages: [
+            {value: 'Hello micro-services world!'},
+        ],
+
+    });
+    // Disconnect the producer once we're done
+    await producer.disconnect();
+})()
+
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`🎉🎉🎉 Application running on port: ${PORT} 🎉🎉🎉`);
 });
